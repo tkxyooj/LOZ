14b004c996bad913201561120ca9decbb25b1b2d
tkxyooj <tkxyooj@gmail.com>
2018-07-05 15:37:18 -0700

    Update pegasusboots.java

--- java/tkxyooj/LOZ/items/new/pegasusboots.java
+++ java/tkxyooj/LOZ/items/new/pegasusboots.java
@@ -1,75 +1,64 @@
-public class ItemHeroArmor {
-
-    public ItemHeroArmor() {
+public class ItemPegasusBoots extends ItemArmor
+{
+    public static final ArmorMaterial pegasusboots = EnumHelper.addArmorMaterial("PEGASUSBOOTS", "pegasus_boots", (int)Double.POSITIVE_INFINITY, new int[]{6, 16, 12, 6}, 1000, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 3.0f);
+    public final EntityEquipmentSlot slot;
+    
+    public ItemPegasusBoots(EntityEquipmentSlot slot) 
+    {
+        super(pegasusboots, 0, slot);
+        this.slot = slot;
+        setCreativeTab(LOZ.tab);
     }
 
-    public static Item helmet;
-    public static Item body;
-    public static Item legs;
-    public static Item boots;
-
-    public void load(FMLInitializationEvent event) {
-        if (event.getSide() == Side.CLIENT) {
-            Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(helmet, 0, new ModelResourceLocation("LOZmod:heroarmorhelmet", "inventory"));
-            Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(body, 0, new ModelResourceLocation("LOZmod:heroarmorbody", "inventory"));
-            Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(legs, 0, new ModelResourceLocation("LOZmod:heroarmorlegs", "inventory"));
-            Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(boots, 0, new ModelResourceLocation("LOZmod:heroarmorboots", "inventory"));
-        }
-        helmet.setCreativeTab(LOZ.tab);
-        body.setCreativeTab(LOZ.tab);
-        legs.setCreativeTab(LOZ.tab);
-        boots.setCreativeTab(LOZ.tab);
-    }
-
-    public void serverLoad(FMLServerStartingEvent event) {
-    }
-
-    public void preInit(FMLPreInitializationEvent event) {
-    }
-
-    public void registerRenderers() {
+    @Override
+    public void setDamage(ItemStack stack, int damage) {
+        super.setDamage(stack, 0);
     }
     
-    static {
-        ItemArmor.ArmorMaterial enuma = EnumHelper.addArmorMaterial("HEROARMOR", "hero_tunic", (int)Double.POSITIVE_INFINITY, new int[]{6, 16, 12, 6}, 1000, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 3.0f);
-        int armorPreffix = 0;
-
-        helmet = (new ItemArmor(enuma, armorPreffix, EntityEquipmentSlot.HEAD) {
-            public void onArmorTick(World world, EntityPlayer entity, ItemStack itemstack) {
-                entity.addPotionEffect(new PotionEffect(MobEffects.HASTE, (int) 3600, (int) 1));
-            }
-        }).setUnlocalizedName("heroarmorhelmet");
-        helmet.setMaxStackSize(1);
-        helmet.setMaxDamage(0);
-        
-        body = (new ItemArmor(enuma, armorPreffix, EntityEquipmentSlot.CHEST) {
-            public void onArmorTick(World world, EntityPlayer entity, ItemStack itemstack) {
-                entity.addPotionEffect(new PotionEffect(MobEffects.REGENERATION, (int) 3600, (int) 1));
-            }
-        }).setUnlocalizedName("heroarmorbody");
-        body.setMaxStackSize(1);
-        body.setMaxDamage(0);
-        
-        legs = (new ItemArmor(enuma, armorPreffix, EntityEquipmentSlot.LEGS) {
-            public void onArmorTick(World world, EntityPlayer entity, ItemStack itemstack) {}
-        }).setUnlocalizedName("heroarmorlegs");
-        legs.setMaxStackSize(1);
-        legs.setMaxDamage(0);
-        
-        boots = (new ItemArmor(enuma, armorPreffix, EntityEquipmentSlot.FEET) {
-            public void onArmorTick(World world, EntityPlayer entity, ItemStack itemstack) {}
-        }).setUnlocalizedName("heroarmorboots");
-        boots.setMaxStackSize(1);
-        boots.setMaxDamage(0);
-        
-        helmet.setRegistryName("heroarmorhelmet");
-        body.setRegistryName("heroarmorbody");
-        legs.setRegistryName("heroarmorlegs");
-        boots.setRegistryName("heroarmorboots");
-        
-        ForgeRegistries.ITEMS.register(helmet);
-        ForgeRegistries.ITEMS.register(body);
-        ForgeRegistries.ITEMS.register(legs);
-        ForgeRegistries.ITEMS.register(boots);
+    @Override
+    public void setMaxStackSize(int amount)
+    {
+        super.setMaxStackSize(1);
     }
+    
+    @Override
+    public void setMaxDamage(int amount)
+    {
+        super.setMaxDamage(0);
+    }
+    
+    @Override
+    public void onArmorTick(World world, EntityPlayer player, ItemStack itemStack) 
+    {
+        boolean flying = player.capabilities.isFlying;
+		if (player.onGround || flying) 
+        {
+			boolean sneaking = player.isSneaking();
+			float speed = 0.15f 
+			* (flying ? 1.1f : 1.0f) 
+			* (sneaking ? 0.1f : 1.0f); 
+							
+			if (player.moveForward > 0f) 
+            {
+				player.moveFlying(0f, 1f, speed);
+			} 
+            else if (player.moveForward < 0f) 
+            {
+				player.moveFlying(0f, 1f, -speed * 0.3f);
+			}
+            
+			if (player.moveStrafing != 0f) 
+            {
+				player.moveFlying(1f, 0f, speed * 0.5f * Math.signum(player.moveStrafing));
+			}
+        }
+    }
+    
+    @Override
+    @SideOnly (Side.CLIENT)
+    public boolean hasEffect(ItemStack par1ItemStack) 
+    {
+        return false;
+    }
+
 }
