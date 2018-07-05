6e2bb97ad5571c86967f9506fbf067eeff7f9cfe
tkxyooj <tkxyooj@gmail.com>
2018-07-05 14:47:10 -0700

    Update CommonProxy-new.java

--- java/tkxyooj/LOZ/proxy/CommonProxy-new.java
+++ java/tkxyooj/LOZ/proxy/CommonProxy-new.java
@@ -6,18 +6,6 @@ public class CommonProxy {
         ModBlocks.init();
         ModEntities.init();
 	ModSounds.init();
-    
-    //Sounf registration
-    ResourceLocation healing = new ResourceLocation("lozmod", "lozmod.music.sound.healing");
-		ForgeRegistries.SOUND_EVENTS.register(new net.minecraft.util.SoundEvent(healing).setRegistryName(healing));
-		ResourceLocation soaring = new ResourceLocation("lozmod", "lozmod.music.sound.soaring");
-		ForgeRegistries.SOUND_EVENTS.register(new net.minecraft.util.SoundEvent(soaring).setRegistryName(soaring));
-		ResourceLocation storm = new ResourceLocation("lozmod", "lozmod.music.sound.storm");
-		ForgeRegistries.SOUND_EVENTS.register(new net.minecraft.util.SoundEvent(storm).setRegistryName(storm));
-		ResourceLocation sun = new ResourceLocation("lozmod", "lozmod.music.sound.sun");
-		ForgeRegistries.SOUND_EVENTS.register(new net.minecraft.util.SoundEvent(sun).setRegistryName(sun));
-		ResourceLocation lullaby = new ResourceLocation("lozmod", "lozmod.music.sound.lullaby");
-    ForgeRegistries.SOUND_EVENTS.register(new net.minecraft.util.SoundEvent(lullaby).setRegistryName(lullaby));
     }
 
     public void init(FMLInitializationEvent event) {
