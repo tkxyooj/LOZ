8d0db56b2f48970b8b856b480ae1775a09e02cd9
tkxyooj <tkxyooj@gmail.com>
2018-07-09 08:23:10 -0700

    Create RecipeManager.java

---
+++ java/tkxyooj/LOZ/recipe/RecipeManager.java
@@ -0,0 +1,8 @@
+public class RecipeManager {
+    //TODO: List out all resource location
+    private static final Map<ResourceLocation, IExtremeRecipe> EXTREME_RECIPES = new HashMap<>();
+    
+    public static void init() {
+        //TODO: Register all recipes
+    }
+}
