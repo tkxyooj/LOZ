ca35864d40b88e27462c60c9b5aeffec1ecbff36
tkxyooj <tkxyooj@gmail.com>
2018-07-10 15:26:07 -0700

    Update hearts.java

--- java/tkxyooj/LOZ/items/new/hearts.java
+++ java/tkxyooj/LOZ/items/new/hearts.java
@@ -1,1 +1,20 @@
+public class ItemPickupOnly extends Item
+{
+	public ItemPickupOnly() 
+  {
+		super();
+		setMaxStackSize(1);
+	}
 
+  @Override
+	public boolean onPickupItem(ItemStack stack, EntityPlayer player) 
+  {
+			if (player.getHealth() < player.getMaxHealth() || Config.alwaysPickupHearts()) 
+      {
+			  player.heal(1.0F);
+			  --stack.stackSize;
+				return true;
+			}
+			return false;
+	}
+}
