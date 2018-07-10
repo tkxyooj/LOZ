ffe31a7343b98d90a13df9fcff6af1ed405b6d62
tkxyooj <tkxyooj@gmail.com>
2018-07-10 16:19:53 -0700

    Update ItemHeartContainer.java

--- java/tkxyooj/LOZ/items/new/ItemHeartContainer.java
+++ java/tkxyooj/LOZ/items/new/ItemHeartContainer.java
@@ -1,20 +1,46 @@
 public class ItemHeartsContainer extends Item
 {
-	public ItemPickupOnly() 
-  {
+	public ItemHeartsContainer() 
+  	{
 		super();
 		setMaxStackSize(1);
+		setUnlocalizedName("heartcontainer");
+		setRegistryName("heartcontainer");
+		ForgeRegistries.ITEMS.register(this);
 	}
 
-  @Override
-	public boolean onPickupItem(ItemStack stack, EntityPlayer player) 
-  {
-			if (player.getHealth() < player.getMaxHealth() || Config.alwaysPickupHearts()) 
-      {
-			  player.heal(1.0F);
-			  --stack.stackSize;
-				return true;
+  	@Override
+	public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player) 
+	{
+		if (player.capabilities.isCreativeMode) 
+		{
+			return stack;
+		}
+		if (!world.isRemote && Config.enabledHealthModification()) 
+		{
+			if (Config.getMaxHearts() == 20 && player.getMaxHealth() < 20 )
+			{
+				--stack.stackSize;
+				updateHealth(player);
 			}
-			return false;
+			if (Config.getMaxHearts() == 40 && player.getMaxHealth() < 40 )
+			{
+				--stack.stackSize;
+				updateHealth(player);
+			}
+			if (Config.getMaxHearts() == (int)Double.POSITIVE_INFINITY )
+			{
+				--stack.stackSize;
+				updateHealth(player);
+			}
+		}
+		return stack;
+	}
+	
+	public void updateHealth(EntityPlayer player, int amount)
+	{
+		int amount = 2; //2 health points to give player a full heart
+		player.setMaxHealth(maxHealth + amount);
+		player.heal(player.getMaxHealth());
 	}
 }
