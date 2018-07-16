7901dbd774936e7ae2df7b3a0cf82d1637a93dcf
tkxyooj <tkxyooj@gmail.com>
2018-07-16 07:48:22 -0700

    Add files via upload

---
+++ java/tkxyooj/LOZ_2.0/sounds/StartupCommon.java
@@ -0,0 +1,28 @@
+package tkxyooj.LOZ.sounds;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
+
+public class StartupCommon
+{
+
+	public static void preInitCommon() 
+	{
+
+		//Resource location
+        ResourceLocation healing = new ResourceLocation("lozmod", "lozmod.music.sound.healing");
+        ResourceLocation soaring = new ResourceLocation("lozmod", "lozmod.music.sound.soaring");
+		ResourceLocation storm = new ResourceLocation("lozmod", "lozmod.music.sound.storm");
+		ResourceLocation sun = new ResourceLocation("lozmod", "lozmod.music.sound.sun");
+		ResourceLocation lullaby = new ResourceLocation("lozmod", "lozmod.music.sound.lullaby");
+        
+        //Registers sound
+        ForgeRegistries.SOUND_EVENTS.register(new net.minecraft.util.SoundEvent(healing).setRegistryName(healing));
+        ForgeRegistries.SOUND_EVENTS.register(new net.minecraft.util.SoundEvent(soaring).setRegistryName(soaring));
+        ForgeRegistries.SOUND_EVENTS.register(new net.minecraft.util.SoundEvent(storm).setRegistryName(storm));
+        ForgeRegistries.SOUND_EVENTS.register(new net.minecraft.util.SoundEvent(sun).setRegistryName(sun));
+        ForgeRegistries.SOUND_EVENTS.register(new net.minecraft.util.SoundEvent(lullaby).setRegistryName(lullaby));
+		
+	}
+	
+}
